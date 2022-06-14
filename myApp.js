6aeb326ba4e5bbd7cf3566790fcf49e2f6f34902
matitk <matitk@gmail.com>
2022-06-14 20:15:52 -0300

    Add .env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
-let express = require('express');
-let app = express();
+let express = require('express')
+let app = express()
+require('dotenv').config()
 
 console.log('Hello World')
 
@@ -11,7 +12,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-  res.json({message: 'Hello json'})
+  if (process.env.MESSAGE_STYLE = 'uppercase') {
+    res.json({ message: 'Hello json'.toUpperCase() })
+  } else {
+    res.json({ message: 'Hello json' })
+  }
 })
 
 
