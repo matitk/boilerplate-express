5d973959307491796698dcd827e05b719912143d
matitk <matitk@gmail.com>
2022-06-14 20:34:44 -0300

    Add message style option configurable at .env file

--- myApp.js
+++ myApp.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-  if (process.env.MESSAGE_STYLE = 'uppercase') {
+  const msgStyle = process.env.MESSAGE_STYLE
+  if (msgStyle = 'uppercase') {
     res.json({ message: 'Hello json'.toUpperCase() })
   } else {
     res.json({ message: 'Hello json' })
