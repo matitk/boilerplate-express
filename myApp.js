3518493f9356d6ab28ab756c358b05674761a741
matitk <matitk@gmail.com>
2022-06-17 17:38:09 -0300

    Add Logger feature

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require('express')
 let app = express()
+let logger = require('./services/logger')
 require('dotenv').config()
 
 console.log('Hello World')
 
 app.use('/public', express.static(__dirname + '/public'))
+
+app.use(logger)
 
 app.get('/', (req, res) => {
   const index = __dirname + '/views/index.html'
@@ -13,10 +16,11 @@ app.get('/', (req, res) => {
 
 app.get('/json', (req, res) => {
   const msgStyle = process.env.MESSAGE_STYLE
+  const msg = 'Hello json'
   if (msgStyle === 'uppercase') {
-    res.json({ message: 'Hello json'.toUpperCase() })
+    res.json({message: msg.toUpperCase()})
   } else {
-    res.json({ message: 'Hello json' })
+    res.json({message: msg})
   }
 })
 
