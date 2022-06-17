befb09779972c4b30194f005143c38ef3d0a18f8
matitk <matitk@gmail.com>
2022-06-17 18:14:46 -0300

    Add '/now' route

--- myApp.js
+++ myApp.js
@@ -24,6 +24,13 @@ app.get('/json', (req, res) => {
   }
 })
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString()
+  next()
+}, (req, res) => {
+  res.json({time: req.time})
+})
+
 
 
 
